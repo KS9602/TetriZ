b27a45066b0660089cf2b5bd5057458e034aef8c
KS9602 <kamil.stochmal2@gmail.com>
2023-06-16 20:18:04 +0200

    add logger class

---
+++ BoardLogger.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+class BoardLogger:
+
+    def __init__(self):
+        self.full_log: str = ""
+        self.current_log: str = ""
+        self.score = 0
+
+    def _clear(self) -> None:
+        self._score = 0
+        self.full_log = ""
+        self.current_log = ""
+
+    def add_log(self, board: np.array) -> None:
+        self.current_log = ""
+        for row in board:
+            for i in row:
+                self.full_log += str(i)
+                self.current_log += str(i)
+        self.current_log += ";"
