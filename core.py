16d49c8dda5f227a227ba7d26a465ad3deffacc5
KS9602 <kamil.stochmal2@gmail.com>
2023-06-18 15:47:48 +0200

    add game_engine file

--- core.py
+++ core.py
@@ -1,6 +1,4 @@
 import pygame as pg
-from sys import exit
-from random import choice
 
 from blocks.IBlock import IBlock
 from blocks.SBlock import SBlock
@@ -14,10 +12,8 @@ from board.move_manager import MoveManager
 from player import Player
 from board_logger import BoardLogger
 from controller.event_manager import EventManager
-
-from DrawManager.DrawManager import DrawManager
-
-
+from draw_manager.draw_manager import DrawManager
+from game_engine.game_engine import GameEngine
 
 class Game:
 
@@ -52,98 +48,12 @@ class Game:
         self.draw_manager = DrawManager(window=self.window)
 
         self.event_manager = EventManager(self)
+        self.game_engine = GameEngine(self)
 
+    def run(self):
         while True:
-
-
-
-
-            # pos = pg.mouse.get_pos()
-            # for event in pg.event.get():
-            #     if event.type == pg.QUIT:
-            #         pg.quit()
-            #         exit()
-            #     if (
-            #         event.type == pg.KEYDOWN
-            #         and event.key == pg.K_LEFT
-            #         and self.start_stop_flag == 2
-            #         and self.game_over_flag == 0
-            #     ):
-            #         self.move_manager.draw_move_left(self.figure.move_left(self.board.board))
-            #     if (
-            #         event.type == pg.KEYDOWN
-            #         and event.key == pg.K_RIGHT
-            #         and self.start_stop_flag == 2
-            #         and self.game_over_flag == 0
-            #     ):
-            #         self.move_manager.draw_move_right(self.figure.move_right(self.board.board))
-            #     if (
-            #         event.type == pg.KEYDOWN
-            #         and event.key == pg.K_UP
-            #         and self.start_stop_flag == 2
-            #         and self.game_over_flag == 0
-            #     ):
-            #         self.move_manager.draw_change(self.figure.change_shape(self.board.board))
-            #     if (
-            #         event.type == pg.KEYDOWN
-            #         and event.key == pg.K_DOWN
-            #         and self.start_stop_flag == 2
-            #         and self.game_over_flag == 0
-            #     ):
-            #         self.move_manager.draw_move_down(self.figure.move_down(self.board.board))
-            #     if (
-            #         event.type == pg.MOUSEBUTTONDOWN
-            #         and event.button == 1
-            #         and START_BUTTON.collidepoint(pos)
-            #     ):
-            #         self.start_stop_flag = self.start_stop_flag % 2 + 1
-            #     if (
-            #         event.type == pg.MOUSEBUTTONDOWN
-            #         and event.button == 1
-            #         and PLAYER.collidepoint(pos)
-            #     ):
-            #         self.color_flag = self.color_flag % 2 + 1
-            #     if (
-            #         event.type == pg.MOUSEBUTTONDOWN
-            #         and event.button == 1
-            #         and RESTART.collidepoint(pos)
-            #     ):
-            #         self.start_stop_flag = 1
-            #         self.board = Board()
-            #         self.logger._clear()
-            #         self.figure = None
-            #     if event.type == pg.KEYDOWN:
-            #         if event.key == pg.K_BACKSPACE:
-            #             self.player.nick = self.player.nick[:-1]
-            #         else:
-            #             if len(self.player.nick) < 19:
-            #                 self.player.nick += event.unicode
-
-
-
-            if self.start_stop_flag != 1 and self.game_over_flag == 0:
-                self.delta_fall += self.clock.tick() / 1000
-                while self.delta_fall > 0.25 and self.game_over_flag == 0:
-                    self.logger.add_log(self.board.board)
-                    if 1 not in self.board.board:
-                        block = choice(self.BLOCKS)
-                        self.figure = block()
-
-                        for i in self.figure.box:
-                            if self.board.board[i[0] + 1, i[1]] not in (0, 1):
-                                self.game_over_flag = 1  # gameover
-
-
-                        for i in self.figure.box:
-                            self.board.board[i[0], i[1]] = 1
-                        self.delta_fall = 0.0
-                        self.logger.score += 10
-                    else:
-                        cords = self.figure.falling(self.board.board)
-                        Game.fall_checking(self, cords=cords)
-
+            self.game_engine.start_stop_loop()
             self.event_manager.check_event_type(pg.event.get())
-
             self.draw_manager.draw_boxes(self.board.board,self.figure)
             self.draw_manager.draw_menu(
                 color_flag=self.color_flag,
@@ -154,23 +64,10 @@ class Game:
             pg.display.flip()
 
 
-    def fall_checking(self, cords: list) -> None:
-        """Fall_checking method checks whether figure can continue to fall (if block below is not fall figure or free space,
-        it cant). Then figure is freezed and if blocks create line on all row, row disapear.
-        """
-
-        for i in cords:
-            if self.board.board[i[0], i[1]] not in (0, 1):
-                self.move_manager.freez_figure(cords, self.figure.block_id)
-                self.board.one_line_disapear(self.player)
-                return
-
-        self.move_manager.draw_falling(cords)
-        self.delta_fall = 0.0
-
 
 
 
 if __name__ == "__main__":
     game = Game()
+    game.run()
 
---
+++ game_engine/game_engine.py
@@ -0,0 +1,56 @@
+from random import choice
+
+class GameEngine:
+    def __init__(self,game) -> None:
+        self.game = game
+
+    def main_loop(self):
+        """Main game loop"""
+        self.game.delta_fall += self.game.clock.tick()/1000
+        while self.game.delta_fall > 0.25 and self.game.start_stop_flag == 2 and self.game.game_over_flag == 0:
+            self.game.logger.add_log(self.game.board.board)
+            self.create_figure()
+
+
+    def create_figure(self) -> None:
+        """Create new figure on bard or run falling function"""
+
+        if 1 not in self.game.board.board:
+            block = choice(self.game.BLOCKS)
+            self.game.figure = block()
+            self.game_over_check()
+            self.stack_figure()
+        else:
+            cords = self.game.figure.falling(self.game.board.board)
+            self.fall_checking(cords)
+
+
+    def game_over_check(self) -> None:
+        """Check space, if there is no empty box or self figure box, then game is over"""
+
+        for i in self.game.figure.box:
+            if self.game.board.board[i[0] + 1, i[1]] not in (0, 1):
+                self.game.game_over_flag = 1
+
+    def stack_figure(self) -> None:
+            """ Set falling figure at start possition"""
+
+            for i in self.game.figure.box:
+                self.game.board.board[i[0], i[1]] = 1
+            self.game.delta_fall = 0.0
+            self.game.logger.score += 10
+
+    def fall_checking(self, cords: list) -> None:
+        """Fall_checking method checks whether figure can continue to fall (if block below is not fall figure or free space,
+        it cant). Then figure is freezed and if blocks create line on all row, row disapear.
+        """
+
+        for i in cords:
+            if self.game.board.board[i[0], i[1]] not in (0, 1):
+                self.game.move_manager.freez_figure(cords, self.game.figure.block_id)
+                self.game.board.one_line_disapear(self.game.player)
+                return
+
+        self.game.move_manager.draw_falling(cords)
+        self.game.delta_fall = 0.0
+
